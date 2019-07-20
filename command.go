eeb91073a3767a6b16f7f0207e5b898c909c4e76
Vitali Baumtrok <vbsw@mailbox.org>
2019-07-20 17:40:17 +0200

    help output corrected

--- command.go
+++ command.go
@@ -84,7 +84,7 @@ func (cmd *cmdParser) interpretOneArgument(results *clResults) {
 		cmd.message = cmd.message + "  -v           print version\n"
 		cmd.message = cmd.message + "  --copyright  print copyright\n\n"
 		cmd.message = cmd.message + "SPLIT-CONCATENATE\n"
-		cmd.message = cmd.message + "  fsplit [COMMAND] INPUT-FILE [OUTPUT-FILE/-DIRECTORY]\n\n"
+		cmd.message = cmd.message + "  fsplit [COMMAND] INPUT-FILE [OUTPUT-FILE]\n\n"
 		cmd.message = cmd.message + "COMMAND\n"
 		cmd.message = cmd.message + "  -p=N         split file into N chunks (parts)\n"
 		cmd.message = cmd.message + "  -b=N[U]      split file into N bytes per chunk, U = unit (k/K, m/M or g/G)\n"
